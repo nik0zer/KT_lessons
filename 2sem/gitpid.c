2d58dc64a91bb65d7bcfc894664f43ea8a6c1d70
nik0zer <s21b1.nikozer@gmail.com>
2022-09-09 16:20:14 +0300

    fork error catch

--- 2sem/gitpid.c
+++ 2sem/gitpid.c
@@ -8,12 +8,16 @@ int main()
     int ppid = 0;
     int pidded = 0;
     pidded = getpid();
-    fork();
-    pid = getpid();
-    ppid = getppid();
-    if(pidded == ppid)
-        printf("Child:\nprocrid = %d\nparprocpid = %d\n", pid, ppid);
+    if(fork() != -1)
+    {
+        pid = getpid();
+        ppid = getppid();
+        if(pidded == ppid)
+            printf("Child:\nprocrid = %d\nparprocpid = %d\n", pid, ppid);
+        else
+            printf("Parent:\nprocrid = %d\nparprocpid = %d\n", pid, ppid);
+    }
     else
-        printf("Parent:\nprocrid = %d\nparprocpid = %d\n", pid, ppid);
+        printf("FORK_ERROR");
     return 0;
 }
